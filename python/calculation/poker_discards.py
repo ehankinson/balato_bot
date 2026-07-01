b6871769207da1d52a7c919e8e055c939fd49d1f
Ethan Hankinson <esh64887@gmail.com>
2026-07-01 15:52:41 -0300

    simplified the x of a kind and flush

--- python/calculation/poker_discards.py
+++ python/calculation/poker_discards.py
@@ -4,100 +4,65 @@ from core.enums import Rank, Suit
 from core.models import Card, Deck
 
 
+def calculate_best_odds(
+    bucket: list[int],
+    deck_lookup: dict[int, list[Card]],
+    total_cards: int,
+    amount_to_look_for: int,
+) -> tuple[float, int]:
+    total_draw_combos = math.comb(total_cards, 5)
+    best_prob = float("-inf")
+    best_suit = -1
+
+    for val, val_count in enumerate(bucket):
+        if val_count == amount_to_look_for:
+            continue
+
+        cards_needed = amount_to_look_for - val_count
+        deck_val_total = len(deck_lookup[val])
+        if deck_val_total < cards_needed:
+            continue
+
+        non_val_cards = total_cards - deck_val_total
+        total_probability = 0.0
+
+        end_iter = 5 if deck_val_total >= 5 else deck_val_total
+        for amount in range(cards_needed, end_iter + 1):
+            total_probability += (
+                math.comb(deck_val_total, amount)
+                * math.comb(non_val_cards, 5 - amount)
+                / total_draw_combos
+            )
+
+        if total_probability > best_prob:
+            best_prob = total_probability
+            best_suit = val
+
+    return best_prob, best_suit
 
 
-
-def flush_odds(deck: Deck, dealt_cards: list[Card]) -> tuple[list[Card], float]:
+def flush_odds(deck: Deck, dealt_cards: list[Card]) -> tuple[list[Card], float, int]:
     suit_bucket = [0] * 4
     for card in dealt_cards:
         suit_bucket[card.suit] += 1
 
-    total_cards = deck.total_cards
-    total_draw_combos = math.comb(total_cards, 5)
+    suit_prob, suit = calculate_best_odds(suit_bucket, deck.suits, deck.total_cards, 5)
+    cards_to_discard = [card for card in dealt_cards if card.suit != suit]
 
-    best_prob = float("-inf")
-    best_suit = -1
+    return cards_to_discard, suit_prob, suit
 
-    for suit, dealt_suit_count in enumerate(suit_bucket):
-        deck_suit_total = len(deck.suits[suit])
-        total_probability = 0.0
-        amount_needed_for_flush = 5 - dealt_suit_count
-        non_flush_cards = total_cards - deck_suit_total
-        
-        for amount in range(amount_needed_for_flush, 6):    
-            total_probability += (
-                math.comb(deck_suit_total, amount)
-                * math.comb(non_flush_cards, 5 - amount)
-                / total_draw_combos
-            )
 
-        if total_probability > best_prob:
-            best_prob = total_probability
-            best_suit = suit
-
-    cards_to_discard = []
+def x_of_a_kind_odds(
+    deck: Deck, dealt_cards: list[Card], x: int
+) -> tuple[list[Card], float, int]:
+    rank_bucket = [0] * 13
     for card in dealt_cards:
-        if card.suit == best_suit:
-            continue
+        rank_bucket[card.rank] += 1
 
-        cards_to_discard.append(card)
+    rank_prob, rank = calculate_best_odds(rank_bucket, deck.ranks, deck.total_cards, x)
+    cards_to_discard = [card for card in dealt_cards if card.rank != rank]
 
-    return cards_to_discard, best_prob
-
-
-def x_of_a_kind_odds(deck: Deck, dealt_cards: list[Card], x: int) -> tuple[list[Card], float, int]:
-    bucket = [0] * 13
-    for card in dealt_cards:
-        bucket[card.rank] += 1
-
-    best_prob = float("-inf")
-    best_rank = -1
-    
-    total_cards = deck.total_cards
-    total_draw_combos = math.comb(total_cards, 5)
-
-    for rank, rank_amount in enumerate(bucket):
-        # if we already have x of a kind for this card
-        # just skip since we already have what we are looking for
-        if rank_amount >= x:
-            continue
-            
-        cards_needed = x - rank_amount
-        deck_rank_total = len(deck.ranks[rank])
-        non_rank_total = total_cards - deck_rank_total
-        total_probability = 0.0
-
-        if deck_rank_total < cards_needed:
-            # there is no enought cards left in the deck
-            # to get this
-            continue
-
-        for fetch_amount in range(cards_needed, deck_rank_total + 1):
-            total_probability += (
-                math.comb(deck_rank_total, fetch_amount)
-                * math.comb(non_rank_total, 5 - fetch_amount)
-                / total_draw_combos
-            )
-
-        if total_probability > best_prob:
-            best_prob = total_probability
-            best_rank = rank
-
-    cards_to_discard = []
-    for card in dealt_cards:
-        if len(cards_to_discard) == 5:
-            break
-
-        if card.rank == best_rank:
-            continue
-    
-        cards_to_discard.append(card)
-
-    return cards_to_discard, best_prob, best_rank
-        
-    
-    
-        
+    return cards_to_discard, rank_prob, rank
 
 
 if __name__ == "__main__":
@@ -139,7 +104,7 @@ if __name__ == "__main__":
     ]
 
     deck.filter(hand)
-    flust_discard, prob = flush_odds(deck, hand)
+    flust_discard, prob, suit = flush_odds(deck, hand)
     x = 4
     pair_discard, prob, card = x_of_a_kind_odds(deck, hand, x)
     print(f"The prob for a {x} of a kind is: {prob}")
